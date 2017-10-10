8d33d566e888a2a18a1aff426e00fd68d8a76b5b
madisonr9 <32672766+madisonr9@users.noreply.github.com>
2017-10-10 07:52:31 -0400

    First Commit

---
+++ personality_survey.py
@@ -0,0 +1,9 @@
+print("What's your favorite animal?")
+animal = input()
+
+if animal == "monkey":
+    print("Yay me too!")
+elif animal == "dog":
+    print("That's okay I still like you")
+else:
+    print(animal + "? That's Okay.")
