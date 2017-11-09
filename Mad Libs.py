f0da4bdde99a3237b258d76d7fb8ba406e44f81e
madisonr9 <32672766+madisonr9@users.noreply.github.com>
2017-11-09 09:05:07 -0500

    Initial commit

---
+++ Mad Libs.py
@@ -0,0 +1,66 @@
+print("Write a noun")
+noun1 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Write a verb")
+verb1 = input()
+
+print("Write an adjective")
+adjective2 = input()
+
+print("Write a verb")
+verb2 = input()
+
+print("write a verb")
+verb3 = input()
+
+print("write a noun")
+noun2 = input()
+
+print("write a noun")
+noun3 = input()
+
+print("write a noun")
+noun4 = input()
+
+print("write a noun")
+noun5 = input()
+
+print("write a noun")
+noun6 = input()
+
+print("write a noun")
+noun7 = input()
+
+print("write a noun")
+noun8 = input()
+
+print("write a noun")
+noun9 = input()
+
+print("write a noun")
+noun10 = input()
+
+print("write a verb")
+verb4 = input()
+
+
+
+
+### MAD LIB ###
+
+print("If you want to learn how to make a pizza follow these simple steps")
+print("to make a pizza you have to start with some " + noun1 + " and some " + noun2 + " to make sure its not sticky")
+print("once you have the " + noun1 + " out you must take a " + noun3 + " and roll out the " + noun1)
+print("Once your " + noun1 + " is all rolled out make sure it is in the shape of an " + noun4)
+print("The next thing you are going to need is a some " + noun5 + " sauce to go on the pizza")
+print("take a " + noun6 + " and " + verb1 + " the sauce until it is evenly spread")
+print("once your " + noun5 + " sauce is evenly spread get out your toppings and put them on")
+print("On my pizza i'm going to put on " + noun7 + " " + noun8 + " " + noun9 + " and " + noun10 + " but you can put on less topping than I did")
+print("once your pizza is looking " + adjective1 + " you can " + verb2 + " it to a pan and " + verb3 + " the pizza in the oven ")
+print("Your pizza should look very " + adjective2 + " and good enough to eat ")
+print(" take you pizza out of the oven and " + verb4 + " and enjoy!") 
+
+
