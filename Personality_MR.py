dde62e4b6d62015cce17ed5c4633be579e4b98f2
madisonr9 <32672766+madisonr9@users.noreply.github.com>
2017-11-09 09:06:03 -0500

    Initial commit

---
+++ Personality_MR.py
@@ -0,0 +1,32 @@
+name = "Madison"
+state = "Wisconsin"
+tvshow  = "How I met your Mother"
+city = "Barcelona"
+book = "Mother Daughter Book Club"
+movie = "Zenon"
+
+print(name + " likes to visit " + state + " and " + city)
+print(name + " loves to watch " + tvshow + " and read " + book)
+
+print(name + " enjoys watching " + movie)
+
+print(" what's your favorite subject?")
+subject = input()
+if subject == "History" or subject == "Spanish":
+    print("Me too!")
+else:
+    print(subject + "that subject is cool too")
+
+print("how old are you")
+age = input()
+if age == "13":
+    print("yay we are the same age")
+else:
+    print("Your okay")
+
+print("Do you like to dance?")
+dance = input()
+
+    
+          
+    
---
+++ Personality_survey_MR.py
@@ -0,0 +1,52 @@
+print("What's your name?")
+name= input()
+
+print("Hello, "+ name)
+
+
+
+print("Whats your favorite color?")
+color = input()
+
+if color == "green" or color == "blue":
+    print(" I also like the color " + color)
+elif color == "yellow":
+    print("I sort of like the color" + color)
+elif color == "pink":
+    print("Im not very fond of that color")
+else:
+    print("I don't really care about that color")
+
+print("What's Your favorite TV show?")
+tvshow = input()
+
+if tvshow == "How I met your mother" or tvshow == "Friends":
+    print("Yay you like old shows like I do. I love " + tvshow)
+elif tvshow == "gossip girl":
+    print("I love that show too!")
+elif tvshow == "greys anatomy":
+    print("Cool! I love " + tvshow + " too")
+elif tvshow == "gilmore girls":
+    print("OMG!! I absolutley love " + tvshow)
+else:
+    print(" Hmmm.. thats cool too")
+
+print("What school do you go to?")
+school = input()
+
+if school == "gcds":
+    print("I go to " + school + " too")
+elif school == "GA" or school == "Brunswick":
+    print("Cool that school is just down the street")
+else:
+    print("I don't think I have heard of that school.")
+
+print("What's your favorite animal?")
+animal = input()
+
+
+
+    
+          
+    
+    
